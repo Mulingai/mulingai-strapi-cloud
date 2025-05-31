41a674fa4470314b311892cfacbe3bf3d03b56d8
Sayid (David) <sayid.malekdoost@gmail.com>
2025-05-31 14:13:36 +0200

    database

--- config/database.ts
+++ config/database.ts
@@ -1,7 +1,7 @@
-import path from 'path';
+import path from 'path'
 
 export default ({ env }) => {
-  const client = env('DATABASE_CLIENT', 'sqlite');
+  const client = env('DATABASE_CLIENT', 'sqlite')
 
   const connections = {
     mysql: {
@@ -11,17 +11,26 @@ export default ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
+        ssl:
+          env.bool('DATABASE_SSL', false) && {
+            key: env('DATABASE_SSL_KEY'),
+            cert: env('DATABASE_SSL_CERT'),
+            ca: env('DATABASE_SSL_CA'),
+            capath: env('DATABASE_SSL_CAPATH'),
+            cipher: env('DATABASE_SSL_CIPHER'),
+            rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+          },
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: {
+        min: env.int('DATABASE_POOL_MIN', 2),
+        max: env.int('DATABASE_POOL_MAX', 10),
+        
+        ...(env.int('DATABASE_POOL_IDLE_TIMEOUT', 0) > 0 && {
+          idleTimeoutMillis: env.int('DATABASE_POOL_IDLE_TIMEOUT'),
+        }),
+      },
     },
+
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
@@ -30,25 +39,31 @@ export default ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-        },
+        ssl:
+          env.bool('DATABASE_SSL', false) && {
+            rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+          },
         schema: env('DATABASE_SCHEMA', 'public'),
+        keepAlive: env.bool('DATABASE_KEEP_ALIVE', true),
+        keepAliveInitialDelayMillis: env.int('DATABASE_KEEP_ALIVE_DELAY', 30000),
       },
-      pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
+      pool: {
+        min: env.int('DATABASE_POOL_MIN', 2),
+        max: env.int('DATABASE_POOL_MAX', 10),
+
+        ...(env.int('DATABASE_POOL_IDLE_TIMEOUT', 0) > 0 && {
+          idleTimeoutMillis: env.int('DATABASE_POOL_IDLE_TIMEOUT'),
+        }),
+      },
     },
+
     sqlite: {
       connection: {
         filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
       },
       useNullAsDefault: true,
     },
-  };
+  }
 
   return {
     connection: {
@@ -56,5 +71,5 @@ export default ({ env }) => {
       ...connections[client],
       acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 600000),
     },
-  };
-};
+  }
+}
